5fa6bfde766b8d2bab39711b2c3cf6a17431c502
Atul kumar singh <atulkumarsingh958@gmail.com>
2025-07-19 00:19:33 +0530

    create nav / footer/

--- frontend/Footer.jsx
+++ frontend/Footer.jsx
@@ -1,7 +1,97 @@
 import React from "react";
 function Footer() {
     return ( <>
-    <h1>footer</h1>
+    <div className="container">
+        <div className="row">
+            <div className="col">
+                <img src="media/image/logo.svg" style={{width: "50%"}}/>
+            </div>
+              <div className="col">
+
+                <p>Company</p>
+                <a href="">Company</a>
+                <br />
+                <a href="">About</a>
+                <br />
+                <a href="">Philosophy</a>
+                <br />
+                <a href="">Press & media </a>
+                <br />
+                <a href="">Careers</a> 
+                <br />
+                <a href="">Zerodha Cares (CSR)</a> 
+                <br />
+                <a href="">Zerodha.tech</a> 
+                <br />
+                <a href=""> Open source </a> 
+
+
+              </div>
+                <div className="col">
+                    <p>Support</p>
+                    
+                  <a href="">Contact us</a> 
+                  <br />
+                  <a href="">Support portal</a>
+                  <br />
+                  <a href="">How to file a complaint?</a>
+                  <br /> 
+                  <a href="">Status of your complaints</a>
+                  <br /> 
+                  <a href="">Bulletin</a> 
+                  <br />
+                  <a href="">Circular</a> 
+                  <br />
+                  <a href="">Z-Connect blog</a>
+                  <br /> 
+                  <a href="">Downloads</a> 
+
+
+
+                </div>
+                  <div className="col">
+                    <p>Account</p>
+                    <a href="">Open demat account</a>
+                    <br />
+                    <a href="">Minor demat account</a>
+                    <br />
+                    <a href="">NRI demat account</a>
+                    <br />
+                    <a href="">Commodity</a>
+                    <br />
+                    <a href="">Dematerialisation</a>
+                    <br />
+                    <a href="">Fund transfer</a>
+                    <br />
+                    <a href="">MTF</a>
+                    <br />
+                    <a href="">Referral program</a>
+        
+
+                  </div>
+
+                  <div className="col">
+                    <p>Quick links</p>
+                    <a href=""> Upcoming IPOs</a>
+                    <br />
+                    <a href="">Brokerage charges</a>
+                    <br />
+                    <a href="">Market holidays </a>
+                    <br />
+                    <a href="">Economic calendar</a>
+                    <br />
+                    <a href="">Calculators</a>
+                    <br />
+                    <a href=""> markets</a>
+                    <br />
+                    <a href=""Sectors></a>
+
+
+
+                  </div>
+        </div>
+
+    </div>
     </> );
 }
 
--- frontend/OpenAccount.jsx
+++ frontend/OpenAccount.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 function OpenAccount() {
     return (<>
-    <h1>
-        OpenAccount
-    </h1>
+   <div className="container p-5">
+    <div className="row text-center">
+        <h1 className="mt-5"> open a Zerodha Account</h1>
+        <p>Modern platform and apps , 0 investments , and flat 20 intraday and F&O trades)</p>
+    
+    <button className="p-2 btn btn-primary fs-5" style={{width: '20%', margin: '0 auto'}}>Sign up Now</button>
+    </div>
+   </div>
     </>  );
 }
 
