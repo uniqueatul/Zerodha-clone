d45685ed8c96501a5b20c3e1d1a002103e81127a
Atul kumar singh <atulkumarsingh958@gmail.com>
2025-07-21 15:53:14 +0530

    landing page complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HomePage from './landing-page/home/HomePage'
-import Navbar from '../Navbar'
-import OpenAccount from '../OpenAccount'
-import Footer from '../Footer'
-import AboutPage from './landing-page/about/AboutPage'
 
-function App() {
-  
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-  return (
-    <>
-      
-     <HomePage/>
-     
+import HomePage from "./landing_page/home/HomePage";
+import Signup from "./landing_page/signup/Signup";
+import AboutPage from "./landing_page/about/AboutPage";
+import ProductPage from "./landing_page/products/ProductsPage";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import SupportPage from "./landing_page/support/SupportPage";
 
-      {/* <AboutPage/> */}
-    </>
-  )
-}
+import NotFound from "./landing_page/NotFound";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
 
-export default App
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/product" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+);
--- frontend/src/landing-page/home/Hero.jsx
+++ frontend/src/landing-page/home/Hero.jsx
@@ -1,19 +1,28 @@
- 
 import React from "react";
 
 function Hero() {
-    return ( <>
-   
-   <div className="container p-5">
-    <div className="row text-center">
-        <img src="media/homeHero.png" alt="Hero image" className="mb-5" />
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img
+          src="media/images/homeHero.png"
+          alt="Hero Image"
+          className="mb-5"
+        />
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, and
+          more
+        </p>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
     </div>
-    <h1 className="mt-5">Invest in everything</h1>
-    <p>online platforn to invest in stocks , derivatives, mutual funds, and more</p>
-    <button className="p-2 btn btn-primary fs-5" style={{width: '20%', margin: '0 auto'}}>Signup Now</button>
-
-   </div>
-    </> );
+  );
 }
 
 export default Hero;
